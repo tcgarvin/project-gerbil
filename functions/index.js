9c10d640cd14bf00e6ead517d96cedf3f7a32b82
Tim Garvin <timothy.garvin@hcl.com>
2018-06-20 00:15:20 -0400

    Trying a Google datastore

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,35 @@
+// Imports the Google Cloud client library
+const Datastore = require('@google-cloud/datastore');
+
 exports.helloWorld = (req, res) => {
-  console.log("It didn't. (3)");
-  res.json({
-    status:"success",
-    result: {
-      "statement": "Hello World! (3)"
-    }
-  });
+  // Creates a client
+  const datastore = new Datastore();
+
+  // The Cloud Datastore key for the new entity
+  const taskKey = datastore.key('Activity');
+
+  // Prepares the new entity
+  const task = {
+    key: taskKey,
+    data: {
+      start: new Date(),
+      status: "active"
+    },
+  };
+
+  // Saves the entity and responds
+  datastore
+    .save(task)
+    .then(() => {
+      console.log(`Saved ${task.key.name}: ${task.data.description}`);
+      res.json({
+        status: "success",
+        result: task.data
+      })
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+      res.status(500).end();
+    });
 };
 
